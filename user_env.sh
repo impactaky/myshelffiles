822a55f485508481617221596b518ec5ebbf78f7
Kohei Nozaki <kohei.nozaki@tx-inc.com>
2025-04-12 21:55:02 +0900

    Import workaround to user_env.sh

--- user_env.sh
+++ user_env.sh
@@ -3,8 +3,3 @@
 ## starship
 export STARSHIP_CONFIG="$XDG_CONFIG_HOME"/starship.toml
 export STARSHIP_CACHE="$XDG_CACHE_HOME"/starship
-
-## zsh
-export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
-
-export SHELFFILES_SHELL=zsh
