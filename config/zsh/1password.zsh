9644ebfca1c18d88da2a8e4483acae7bed389d64
impactaly <impactaky@users.noreply.github.com>
2025-10-05 00:27:49 +0900

    Automate atuin login

--- config/zsh/1password.zsh
+++ config/zsh/1password.zsh
@@ -1,3 +1,14 @@
 export GH_TOKEN="op://shelffiles/GH_TOKEN/credential"
-
 alias gh="op run --no-masking -- gh"
+
+function atuin_login() {
+  ATUIN_USER="op://shelffiles/atuin/username" \
+  ATUIN_PASSWORD="op://shelffiles/atuin/password" \
+  ATUIN_KEY="op://shelffiles/atuin_key/password" \
+  op run --no-masking -- zsh -c '''
+    atuin login \
+      --username "$ATUIN_USER" \
+      --password "$ATUIN_PASSWORD" \
+      --key "$ATUIN_KEY"
+    '''
+}
