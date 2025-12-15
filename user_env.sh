736ecd2e193df0616b2cd58a93470c6de7829d8a
impactaly <impactaky@users.noreply.github.com>
2025-12-15 11:54:42 +0900

    Use packages

--- user_env.sh
+++ user_env.sh
@@ -1,16 +1,6 @@
 # Reference: https://wiki.archlinux.org/title/XDG_Base_Directory
 
-## starship
-export STARSHIP_CONFIG="$XDG_CONFIG_HOME"/starship.toml
-export STARSHIP_CACHE="$XDG_CACHE_HOME"/starship
-
-## npm
-export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
-export PATH="$XDG_DATA_HOME/npm/bin:$PATH"
-
-## claude
-export CLAUDE_CONFIG_DIR="$XDG_CONFIG_HOME/claude"
-
-# deno
-export DENO_INSTALL_ROOT="$XDG_DATA_HOME/deno/bin"
-export PATH="$DENO_INSTALL_ROOT:$PATH"
+. $SHELFFILES/shelffiles/packages/starship.sh
+. $SHELFFILES/shelffiles/packages/nodejs.sh
+. $SHELFFILES/shelffiles/packages/deno.sh
+. $SHELFFILES/shelffiles/packages/claude-code.sh
